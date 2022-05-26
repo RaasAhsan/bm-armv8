39e3d23e287ba4c62258e340a83b33550a35e6d7
Raas Ahsan <raas.ahsan@gmail.com>
2022-05-25 21:52:36 -0500

    reorganize

--- uart.h
+++ uart.h
@@ -3,6 +3,7 @@
 void uart_init();
 void uart_putchar(char c);
 char uart_getchar();
+void uart_clear_interrupts();
+
 void uart_gets(char *buf);
 void uart_puts(const char *s);
-void uart_clear_interrupts();
