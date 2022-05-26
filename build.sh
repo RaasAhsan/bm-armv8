35b4bad04998561bbad8d512c3ef973083ec137c
Raas Ahsan <raas.ahsan@gmail.com>
2022-05-26 02:50:30 -0500

    Cargo workspaces for drivers/rust code

--- build.sh
+++ build.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-cd drivers && \
 cargo build --release && 
-cd ../ && \
 as boot.s -o boot.o && \
 as vector.s -o vector.o && \
 gcc -ffreestanding -c kernel.c -o kernel.o && \
 gcc -ffreestanding -c uart.c -o uart.o && \
 gcc -ffreestanding -c gic.c -o gic.o && \
-ld -nostdlib -T linker.ld boot.o vector.o kernel.o uart.o gic.o drivers/target/aarch64-unknown-none/release/libuart.a -o kernel.elf && \
+ld -nostdlib -T linker.ld boot.o vector.o kernel.o uart.o gic.o target/aarch64-unknown-none/release/libpl011.a -o kernel.elf && \
 objcopy -O binary kernel.elf kernel.bin && \
 objdump -d kernel.elf  > kernel.dump
