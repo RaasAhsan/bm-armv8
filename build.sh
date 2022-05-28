11fb340f6f928bd3cfcd8af54e0a476489c685ae
Raas Ahsan <raas.ahsan@gmail.com>
2022-05-28 05:02:05 -0500

    src directory

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 cargo build --release && \
-as boot.s -o boot.o && \
-as vector.s -o vector.o && \
-gcc -Iinclude -ffreestanding -c kernel.c -o kernel.o && \
-gcc -Iinclude -ffreestanding -c uart.c -o uart.o && \
-gcc -Iinclude -ffreestanding -c gic.c -o gic.o && \
-gcc -Iinclude -ffreestanding -c scheduler.c -o scheduler.o && \
-gcc -Iinclude -ffreestanding -c syscall.c -o syscall.o && \
-gcc -Iinclude -ffreestanding -c process.c -o process.o && \
-ld -nostdlib -T linker.ld boot.o vector.o kernel.o uart.o gic.o scheduler.o syscall.o process.o target/aarch64-unknown-none/release/libkernel.a -o kernel.elf && \
-objcopy -O binary kernel.elf kernel.bin && \
-objdump -d kernel.elf > kernel.dump
+as src/boot.s -o out/boot.o && \
+as src/vector.s -o out/vector.o && \
+gcc -Iinclude -ffreestanding -c src/kernel.c -o out/kernel.o && \
+gcc -Iinclude -ffreestanding -c src/uart.c -o out/uart.o && \
+gcc -Iinclude -ffreestanding -c src/gic.c -o out/gic.o && \
+gcc -Iinclude -ffreestanding -c src/scheduler.c -o out/scheduler.o && \
+gcc -Iinclude -ffreestanding -c src/syscall.c -o out/syscall.o && \
+gcc -Iinclude -ffreestanding -c src/process.c -o out/process.o && \
+ld -nostdlib -T linker.ld out/boot.o out/vector.o out/kernel.o out/uart.o out/gic.o out/scheduler.o out/syscall.o out/process.o target/aarch64-unknown-none/release/libkernel.a -o out/kernel.elf && \
+objcopy -O binary out/kernel.elf out/kernel.bin && \
+objdump -d out/kernel.elf > out/kernel.dump
