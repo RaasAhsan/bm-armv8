10e338e8daf0130a1f4a73ea8d6ec5395ac5343c
Raas Ahsan <raas.ahsan@gmail.com>
2022-06-05 03:55:28 -0500

    Add comment for kmalloc

--- kernel/src/kmalloc.rs
+++ kernel/src/kmalloc.rs
@@ -1,18 +1,19 @@
+// Implements a kernel memory heap via a bump allocator
 
-static mut HEAP_PTR: *mut u8 = 0 as *mut u8;
+static mut HEAP_BASE: *mut u8 = 0 as *mut u8;
 static mut HEAP_OFFSET: usize = 0;
 
 #[no_mangle]
 pub extern "C" fn kmalloc_init(addr: *mut u8) {
     unsafe {
-        HEAP_PTR = addr;
+        HEAP_BASE = addr;
     }
 }
 
 #[no_mangle]
 pub extern "C" fn kmalloc(size: usize) -> *mut u8 {
     unsafe {
-        let ptr: *mut u8 = HEAP_PTR.offset(HEAP_OFFSET as isize);
+        let ptr: *mut u8 = HEAP_BASE.offset(HEAP_OFFSET as isize);
         HEAP_OFFSET += size;
         return ptr;
     }
